f7de830b1aa27491d08799ba1a23b037b21beab2
Benoit Bovy <benbovy@gmail.com>
2019-04-23 17:14:49 +0200

    add basic setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='paraspec',
+      version='0.1.0',
+      description='A simple Parapatric Speciation Model as a LEM component',
+      url='https://gitext.gfz-potsdam.de/sec55-public/parapatric-speciation',
+      author='Katherine Kravitz, Benoit Bovy',
+      author_email='kravitz@gfz-potsdam.de, bbovy@gfz-potsdam.de',
+      license='GPLv3',
+      packages=['paraspec'],
+      install_requires=['numpy', 'scipy', 'pandas'],
+      zip_safe=False)
