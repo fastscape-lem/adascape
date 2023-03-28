c7d9a2641733e2d52a801339e024e84babca766d
EstebanAce <acevedoesteban@gmail.com>
2023-03-28 14:39:19 +0200

    add fastscape dependency

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='adascape',
       author_email='esteban.acevedo-trejos@gfz-potsdam.de, kravitz@gfz-potsdam.de, bbovy@gfz-potsdam.de',
       license='GPLv3',
       packages=['adascape'],
-      install_requires=['numpy', 'scipy', 'pandas', 'dendropy', 'orographic_precipitation'],
+      install_requires=['numpy', 'scipy', 'pandas', 'dendropy', 'fastscape', 'orographic_precipitation'],
       zip_safe=False)
